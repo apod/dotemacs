1da9fb5dab6e2f4033be1ef7d9539b526f190358
Alex Podaras <apodaras@gmail.com>
2015-01-18 19:51:24 +0200

    Add magit

--- core/ap-essentials.el
+++ core/ap-essentials.el
@@ -11,4 +11,9 @@
                 projectile-known-projects-file
                 (expand-file-name "projectile-bookmarks.eld" ap-data-directory)))
 
+;;; Magit
+(use-package magit
+  :ensure t
+  :defer t)
+
 (provide 'ap-essentials)
