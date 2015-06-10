de742296432037aefbb10d30cf0f6c631d268aad
Alex Podaras <apodaras@gmail.com>
2015-06-10 13:27:43 +0300

    Don't sudo when using C-d in ido-find-file

--- core/ap-ido.el
+++ core/ap-ido.el
@@ -9,9 +9,10 @@
   :config (progn
             (defadvice ido-find-file (after find-file-sudo activate)
               "Find file as root if necessary."
-              (unless (and buffer-file-name
-                           (file-writable-p buffer-file-name))
-                (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+              (when (not (eq ido-exit 'dired))
+                (unless (and buffer-file-name
+                             (file-writable-p buffer-file-name))
+                  (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name)))))
 
             (ido-mode t)
             (ido-everywhere t)))
