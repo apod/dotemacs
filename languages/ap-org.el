1de01f96cf1c2a9d5551838c045adb039f82cf06
Alex Podaras <apodaras@gmail.com>
2015-12-16 12:54:12 +0200

    Add some basic evil keybindings for org-mode

--- languages/ap-org.el
+++ languages/ap-org.el
@@ -4,6 +4,12 @@
 (use-package org
   :ensure t
   :defer t
-  :init (setq org-log-done 'time))
+  :init (setq org-log-done 'time)
+  :config (evil-leader/set-key-for-mode 'org-mode
+            "h" 'org-metaleft
+            "j" 'org-metadown
+            "k" 'org-metaup
+            "l" 'org-metaright
+            "mt" 'org-todo))
 
 (provide 'ap-org)
