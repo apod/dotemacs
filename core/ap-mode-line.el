c56adbc8b5baf4941b4408f5ff76a1140e283770
Alex Podaras <apodaras@gmail.com>
2015-12-16 23:49:21 +0200

    Add correct branch to mode-line, vc-mode does not seem to update correctly

--- core/ap-mode-line.el
+++ core/ap-mode-line.el
@@ -9,6 +9,7 @@
 
             (require 'spaceline-segments)
             (spaceline-define-segment ap-evil-state
+              "The current evil state, without the tag brackets."
               (let ((state
                      (replace-regexp-in-string "\\s-*<\\(.+\\)>\\s-*"
                                                " \\1 "
@@ -16,26 +17,39 @@
                 (propertize state 'face 'bold))
               :when (bound-and-true-p evil-local-mode))
 
-            (diminish 'projectile-mode)
             (spaceline-define-segment ap-projectile
+              "The current project name based on projectile."
               (projectile-project-name)
               :when (and (functionp 'projectile-project-name)
                          (not (string= (projectile-project-name) "-"))))
+
+            (spaceline-define-segment ap-git-branch
+              "Current git branch."
+              (or (magit-get-current-branch)
+                  (magit-rev-parse "--short" "HEAD"))
+              :when (and vc-mode
+                         (string= (vc-backend (buffer-file-name (current-buffer))) "Git")
+                         (functionp 'magit-get-current-branch)))
+
+            (spaceline-define-segment ap-line-column
+              "The current line and column numbers."
+              "%2l:%2c")
 
             (spaceline-install
              `((ap-evil-state :face highlight-face)
                (buffer-id
                 buffer-modified)
                (ap-projectile :when active)
-               (version-control :when active))
+               (ap-git-branch :when active
+                              :fallback version-control))
 
              `(major-mode
                (((minor-modes :separator spaceline-minor-modes-separator)
                  process)
                 :when active)
+               buffer-encoding-abbrev
                ((selection-info
-                 point-position
-                 line-column)
+                 ap-line-column)
                 :face highlight-face)))))
 
 (provide 'ap-mode-line)
