22e18b3a3879c7fda4e4d6f0e4ea8eeaae1a95ec
Alex Podaras <apodaras@gmail.com>
2016-06-28 20:02:23 +0300

    Simplify ruby configuration to the stuff i actually use

--- languages/ap-ruby.el
+++ languages/ap-ruby.el
@@ -1,50 +1,19 @@
 ;;; ap-ruby.el
 ;;; Ruby configuration
 
-(use-package enh-ruby-mode
-  :ensure t
-  :defer t
-  :mode (("\\.rb\\'"       . enh-ruby-mode)
-         ("\\.ru\\'"       . enh-ruby-mode)
-         ("\\.gemspec\\'"  . enh-ruby-mode)
-         ("\\.rake\\'"     . enh-ruby-mode)
-         ("Rakefile\\'"    . enh-ruby-mode)
-         ("Gemfile\\'"     . enh-ruby-mode)
-         ("Guardfile\\'"   . enh-ruby-mode)
-         ("Capfile\\'"     . enh-ruby-mode)
-         ("Vagrantfile\\'" . enh-ruby-mode)))
-
-(use-package rbenv
-  :ensure t
-  :defer t
-  :init (setq rbenv-show-active-ruby-in-modeline nil)
-  :config (progn
-            (global-rbenv-mode)
-            (add-hook 'enh-ruby-mode-hook 'rbenv-use-corresponding)))
-
-(use-package inf-ruby
-  :ensure t
-  :defer t
-  :config (evil-leader/set-key-for-mode 'enh-ruby-mode
-            "mr" 'ruby-send-region
-            "mb" 'ruby-send-block))
-
-(use-package robe
-  :ensure t
-  :defer t
-  :config (progn
-            (add-hook 'enh-ruby-mode-hook 'robe-mode)
-            (evil-leader/set-key-for-mode 'enh-ruby-mode
-              "md" 'robe-doc
-              "mz" 'robe-start)))
+(use-package ruby-mode
+  :mode ("\\.rb\\'"
+         "\\.rake\\'"
+         "\\.gemspec\\'"
+         "/\\(Gem\\|Cap\\|Vagrant\\|Rake\\)file\\'")
+  :interpreter "ruby")
 
 (use-package slim-mode
   :ensure t
-  :mode ("\\.slim\\'" . slim-mode))
+  :mode "\\.slim\\'")
 
 (use-package yaml-mode
   :ensure t
-  :mode (("\\.yml\\'"  . yaml-mode)
-         ("\\.yaml\\'" . yaml-mode)))
+  :mode ("\\.yml\\'" "\\.yaml\\'"))
 
 (provide 'ap-ruby)
