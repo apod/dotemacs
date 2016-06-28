51e6524b337b18f23c201ea03a5cd2aa7cb5c115
Alex Podaras <apodaras@gmail.com>
2016-06-28 22:26:59 +0300

    Progn is no longer needed on use-package macro

--- languages/ap-emacs-lisp.el
+++ languages/ap-emacs-lisp.el
@@ -2,24 +2,25 @@
 ;;; Emacs lisp configuration
 
 (use-package lisp-mode
-  :config (progn
-            (defadvice eval-print-last-sexp (around evil activate)
-              "In normal-state, last sexp ends at point."
-              (if (evil-normal-state-p)
-                  (progn
-                    (unless (or (eobp) (eolp)) (forward-char))
-                    ad-do-it)
-                ad-do-it))
+  :config
+  (add-hook 'emacs-lisp-mode-hook (lambda ()
+                                    (evil-smartparens-mode t)
+                                    (eldoc-mode t)))
 
-            (add-hook 'emacs-lisp-mode-hook (lambda ()
-                                              (evil-smartparens-mode t)
-                                              (eldoc-mode t)))
-            (ap-set-key-for-modes (emacs-lisp-mode lisp-interaction-mode)
-              "mi" 'ap-indent-defun
-              "me" 'eval-last-sexp
-              "mp" 'eval-print-last-sexp
-              "mx" 'eval-defun
-              "mr" 'eval-region
-              "mb" 'eval-buffer)))
+  (defadvice eval-print-last-sexp (around evil activate)
+    "In normal-state, last sexp ends at point."
+    (if (evil-normal-state-p)
+        (progn
+          (unless (or (eobp) (eolp)) (forward-char))
+          ad-do-it)
+      ad-do-it))
+
+  (ap-set-key-for-modes (emacs-lisp-mode lisp-interaction-mode)
+                        "mi" 'ap-indent-defun
+                        "me" 'eval-last-sexp
+                        "mp" 'eval-print-last-sexp
+                        "mx" 'eval-defun
+                        "mr" 'eval-region
+                        "mb" 'eval-buffer))
 
 (provide 'ap-emacs-lisp)
