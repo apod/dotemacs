95adf1594d179d3c6a3f8f51fed3803381dde7c5
Alex Podaras <apodaras@gmail.com>
2016-06-28 20:19:42 +0300

    Consistent use of mode

--- languages/ap-markdown.el
+++ languages/ap-markdown.el
@@ -3,8 +3,6 @@
 
 (use-package markdown-mode
   :ensure t
-  :mode (("\\.md\\'"       . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode)))
-
+  :mode ("\\.md\\'" "\\.markdown\\'"))
 
 (provide 'ap-markdown)
