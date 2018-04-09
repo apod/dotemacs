9b03022ac7052aa3d2089938cbe743f07adfb814
Alex Podaras <apodaras@gmail.com>
2018-04-09 12:11:07 +0300

    Add a custom love play fn that uses start-process and locates project root

--- languages/ap-lua.el
+++ languages/ap-lua.el
@@ -13,10 +13,27 @@
     "mx" 'lua-send-defun
     "mz" 'lua-show-process-buffer))
 
+(defun ap-love-play ()
+  (interactive)
+  (let ((buffer-name "*love-output*")
+        (project-dir (locate-dominating-file (file-name-directory (buffer-file-name))
+                                             "main.lua")))
+    (when (get-buffer buffer-name)
+      (kill-buffer buffer-name))
+    (let* ((project-dir (if project-dir
+                            (expand-file-name project-dir)
+                          (file-name-directory (buffer-file-name))))
+           (love-process (start-process "love-process"
+                                        buffer-name
+                                        love-exe
+                                        project-dir)))
+      (message project-dir)
+      (display-buffer (process-buffer love-process)))))
+
 (use-package love-minor-mode
   :ensure t
   :config
   (evil-leader/set-key-for-mode 'lua-mode
-    "m5" 'love/play))
+    "m5" 'ap-love-play))
 
 (provide 'ap-lua)
